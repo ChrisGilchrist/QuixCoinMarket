d2e5f9f2bb767790004ea4aba5d7e02ff73c8c87
Chris Gilchrist <gilly@sgnet.co.uk>
2024-08-06 09:43:31 +0100

    small fix

--- usd-to-gbp-transformation/main.py
+++ usd-to-gbp-transformation/main.py
@@ -29,7 +29,7 @@ def convert_to_gbp(coin_value):
 sdf = (
     # Convert the temperature value from °F to °C
     # E.g. {"tempF": 68} will become {"tempC": 20}
-    sdf.apply(lambda value: {value['symbol']: convert_to_gbp(value)})
+    sdf.apply(lambda value: {iter(value): convert_to_gbp(value)})
 
     # Print the result to the console
     .update(print)
